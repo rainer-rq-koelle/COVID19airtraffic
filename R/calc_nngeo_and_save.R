1e23f1264819dd542feae64a8f94bc0c8d0a8d38
Rainer KOELLE <rainer.koelle@eurocontrol.int>
2021-03-09 16:57:28 +0100

    calc_nngeo_and_save()

---
+++ R/calc_nngeo_and_save.R
@@ -0,0 +1,28 @@
+#' Nearest neighbour and temp save for purrr iteration
+#' 
+#' Wrapper function to have the (i) nearest neighbour and (ii) save while 
+#' iterating over large dataset. This is to reduce the risk of loosing the
+#' work, if things fail or are switched off :)
+#' The OSN data set for 2019 is about 7 mio flights without id for lat/lon.
+#' That runs for days ... 
+#'
+#' @param .sf_pts 
+#' @param .poly world polygons
+#' @param .prefix any code for the tmp folder
+#' @param .pth path to store temp files
+#' @param ... any other useful stuff
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' \donotrun{} calc_nngeo_and_save(add what you need)
+calc_nngeo_and_save<-function(.sf_pts, .poly, .prefix, .pth = pth, ...){
+  # my variables
+  sf_pts <- .sf_ptsf
+  poly <- .poly
+  # kicking off nngeo
+  df <- check_nngeo(sf_pts, poly)
+  # saving out results
+  save_tmp(df, .prefix = .prefix, .pth = .pth)
+}
