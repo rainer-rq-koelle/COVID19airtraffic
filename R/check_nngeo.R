986213a9fe4591197c45a7d1f3380a0f728cc8da
Rainer KOELLE <rainer.koelle@eurocontrol.int>
2021-03-09 16:33:34 +0100

    added DESCRIPTION and turned this into a "half-cooked" package to keep function documentation

--- R/check_nngeo.R
+++ R/check_nngeo.R
@@ -1,14 +1,20 @@
 #' check and assign potential geographic country to lat/lon position
 #'
-#' @param .sf_pts
+#' @param .sf_pts simple feature sf of position points (lat/lon)
+#' @param .poly sf polygon(s) to verify whether points are in or next (.maxdist)
+#'    from polygon (e.g. aircraft position over country or waters close by)
 #' @param .maxdist maximum distance in meters, e.g. 100km = 100000 
 #'
-#' @return
+#' @return country code derived from .poly added to input sf_pts object
 #' @export
 #'
 #' @examples
 check_nngeo <- function(.sf_pts, .poly, .maxdist = 100000, ...){
-  sf_pts <- .sf_pts %>% 
+  # chatty function
+  usethis::ui_info(paste0("crunching batch ", min(.sf_pts$id), "-", max(.sf_pts$id)))
+  
+  # perform spatial join for finding nearest (single) neighbour (k=1) with max distance 
+  sf_pts <- .sf_pts %>%
     sf::st_join(.poly, join = nngeo::st_nn, k = 1, maxdist = .maxdist)
   return(sf_pts)
 }
---
+++ R/save_temp.R
@@ -0,0 +1,31 @@
+#' save temp to store temporary results
+#' 
+#' This utility function saves temporary data prep results as fst into 
+#' a given subfolder of path, i.e. tmp!
+#' The function also coerces the sf object to a normal data frame by 
+#' recovering the lat/lon and dropping the geometry. This allows the tibble
+#' to be saved as fst (binary format).
+#'
+#' @param .ds processed tibble requiring an (additional) id colum
+#' @param .prefix a tag to recall what data subset is processed, e.g. departure side (DEP)
+#' @param .pth path to store the temp files while iterating
+#'
+#' @return no return as file will be written out to pth folder
+#'    the function is chatty (ui_info)
+#' @export
+#'
+#' @examples
+save_tmp <- function(.ds, .prefix, .pth = pth){
+  max_id <- max(.ds$id)
+  my_pth <- paste0(.pth, "tmp/", .prefix,"_", max_id)
+  
+  ds <- .ds %>%
+    # extract lat lon; fst does not support sf (list colums)
+    dplyr::mutate(lat = sf::st_coordinates(.)[,2],
+                  lon = sf::st_coordinates(.)[,1]) %>%
+    sf::st_set_geometry(NULL)
+  
+  fst::write_fst(ds, path = my_pth)
+  usethis::ui_info(paste0("written to ", my_pth))
+}
+
